f9a191af133110a165ea39cc29e12d6cff9fb5cd
Eduardo Moroni <eduardomoroni@gmail.com>
2018-01-04 16:36:42 -0200

    refactor: move express out of index.js

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,15 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
-const cookieParser = require('cookie-parser')();
-const cors = require('cors')({origin: true});
-
 admin.initializeApp(functions.config().firebase);
 
-const app = express();
+const api = require('./api')
+const app = functions.https.onRequest(api);
 
-app.use(cors);
-app.use(cookieParser);
-
-app.get('/', (req, res) => res.send("Hello World"));
-
-exports.app = functions.https.onRequest(app);
-
-exports.date = functions.https.onRequest((req, res) => {
+const date = functions.https.onRequest((req, res) => {
   res.send(new Date())
 });
+
+module.exports = {
+  date,
+  app
+}
